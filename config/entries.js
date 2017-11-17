74729d59ac07fbb1501c97b4961a2934ff64af3f
杨叙 <yangxu@yangxudeMacBook-Pro.local>
2017-11-17 21:08:37 +0800

    111

--- config/entries.js
+++ config/entries.js
@@ -1,72 +1,77 @@
-var path = require('path');
-var fs = require('fs');
-var phone = require('./device');
+var path = require("path");
+var fs = require("fs");
+var phone = require("./device");
 var dirSrc;
 
 // 手机和PC分开
-if(phone !== null) {
-  dirSrc = path.resolve(__dirname, '../src/phone')
-}else {
-  dirSrc = path.resolve(__dirname, '../src/pc')
+if (phone !== null) {
+  dirSrc = path.resolve(__dirname, "../src/phone");
+} else {
+  dirSrc = path.resolve(__dirname, "../src/pc");
 }
 // 单独处理项目首页
 // 因为它的结构与其它页面不同
 var entriesConfig = [
   {
-    entryName: 'index/index',
-    entry: path.resolve(dirSrc, 'index/index.js'),
-    filename: 'index.html',
-    template: path.resolve(dirSrc, 'index.html')
+    entryName: "index/index",
+    entry: path.resolve(dirSrc, "index/index.js"),
+    filename: "index.html",
+    template: path.resolve(dirSrc, "index.html")
   }
 ];
 
-var excludeDirs = ['lib', 'index']
-var dirPages = fs.readdirSync(dirSrc).filter(function (dirName) {
-  return excludeDirs.indexOf(dirName) === -1 && fs.statSync(dirSrc + '/' + dirName).isDirectory()
-})
+var excludeDirs = ["lib", "index"];
+var dirPages = fs.readdirSync(dirSrc).filter(function(dirName) {
+  return (
+    excludeDirs.indexOf(dirName) === -1 &&
+    fs.statSync(dirSrc + "/" + dirName).isDirectory()
+  );
+});
 
-dirPages.forEach(pageWalk)
+dirPages.forEach(pageWalk);
 
-function pageWalk (pageName) {
-  var filemark = 'index'
-  var pagePath = path.resolve(dirSrc, pageName)
-  var files = fs.readdirSync(pagePath)
-  var fileHTML = filemark + '.html'
-  var fileJS = filemark + '.js'
+function pageWalk(pageName) {
+  var filemark = "index";
+  var pagePath = path.resolve(dirSrc, pageName);
+  var files = fs.readdirSync(pagePath);
+  var fileHTML = filemark + ".html";
+  var fileJS = filemark + ".js";
 
-  if (files.indexOf(fileHTML) === -1 || files.indexOf(fileJS) === -1) return
+  if (files.indexOf(fileHTML) === -1 || files.indexOf(fileJS) === -1) return;
 
-  var filename = pageName + '/' + fileHTML
+  var filename = pageName + "/" + fileHTML;
   entriesConfig.push({
-    entryName: pageName + '/' + filemark,
+    entryName: pageName + "/" + filemark,
     entry: path.resolve(dirSrc, pageName, fileJS),
     filename: filename,
     template: path.resolve(dirSrc, filename)
-  })
+  });
 
-  var subDirs = files.filter(function (file) {
-    return fs.statSync(pagePath + '/' + file).isDirectory()
-  }).map(function (dirName) {
-    return pageName + '/' + dirName
-  })
+  var subDirs = files
+    .filter(function(file) {
+      return fs.statSync(pagePath + "/" + file).isDirectory();
+    })
+    .map(function(dirName) {
+      return pageName + "/" + dirName;
+    });
 
   if (subDirs.length) {
-    subDirs.forEach(pageWalk)
+    subDirs.forEach(pageWalk);
   }
 }
 
 module.exports = {
   entries: entriesConfig,
-  assetsRoot: path.resolve(__dirname, '../dist'),
-  assetsSubDirectory: 'assets',
-  commonsChunkName: ['app', 'vendor', 'manifest'],
+  assetsRoot: path.resolve(__dirname, "../dist"),
+  assetsSubDirectory: "assets",
+  commonsChunkName: ["app", "vendor", "manifest"],
   dev: {
-    env: require('./dev.env.js'),
-    assetsPublicPath: '/'
+    env: require("./dev.env.js"),
+    assetsPublicPath: "/"
   },
   build: {
-    env: require('./prod.env.js'),
+    env: require("./prod.env.js"),
     // 可配置 CDN
-    assetsPublicPath: '/'
+    assetsPublicPath: "//"
   }
-}
+};
