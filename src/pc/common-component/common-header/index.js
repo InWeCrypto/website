5cc75be1b3f08ac629ce4acb1cf63cc7f7764e11
Candice <932475428@qq.com>
2017-11-18 15:38:16 +0800

    body style

--- src/pc/common-component/common-header/index.js
+++ src/pc/common-component/common-header/index.js
@@ -1,26 +1,23 @@
-import React from 'react'
-import HomeHeaderTop from './component/home-header-top/index'
-import HomeHeaderNav from './component/home-header-nav/index'
+import React from "react";
+import HomeHeaderTop from "./component/home-header-top/index";
+import HomeHeaderNav from "./component/home-header-nav/index";
 
+import "./index.less";
 
 export default class CommonHeader extends React.Component {
-
   render() {
-    let {classIco, classPrice, classBrowser, classInfo } = this.props;
-    
+    let { classIco, classPrice, classBrowser, classInfo } = this.props;
+
     return (
       <div className="pc-common-header">
         <HomeHeaderTop />
         <HomeHeaderNav
-          classIco = {classIco} 
-          classPrice = {classPrice} 
-          classBrowser = {classBrowser} 
-          classInfo = {classInfo}
-         />
-        
+          classIco={classIco}
+          classPrice={classPrice}
+          classBrowser={classBrowser}
+          classInfo={classInfo}
+        />
       </div>
-    )
+    );
   }
 }
-
-
