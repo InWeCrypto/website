f3767f3d077fd006d47c6328e72bc1c5d876f4c4
杨叙 <yangxu@yangxudeMacBook-Pro.local>
2017-11-18 18:08:55 +0800

    111

--- src/pc/particular-online/component/particular-online-content/index.js
+++ src/pc/particular-online/component/particular-online-content/index.js
@@ -35,10 +35,9 @@ export default class ParticularOnlineContent extends React.Component {
     };
   }
   viewEcharts(nextState) {
-    if (!nextState.oldData) {
+    if (!nextState.optionData) {
       return;
     }
-
     let chart = this.refs.chart;
     let myChart = echarts.init(chart);
     var upColor = "#ec0000";
@@ -47,179 +46,234 @@ export default class ParticularOnlineContent extends React.Component {
     var downBorderColor = "#008F28";
 
     // 数据意义：开盘(open)，收盘(close)，最低(lowest)，最高(highest)
-    var data0 = splitData(nextState.optionData ? nextState.optionData : []);
+    var data = splitData(
+      nextState.optionData
+        ? JSON.parse(JSON.stringify(nextState.optionData))
+        : []
+    );
 
     function splitData(rawData) {
       var categoryData = [];
       var values = [];
+      var volumes = [];
       for (var i = 0; i < rawData.length; i++) {
         categoryData.push(rawData[i].splice(0, 1)[0]);
         values.push(rawData[i]);
+        volumes.push([
+          i,
+          rawData[i][4],
+          rawData[i][0] > rawData[i][1] ? 1 : -1
+        ]);
       }
+
       return {
         categoryData: categoryData,
-        values: values
+        values: values,
+        volumes: volumes
       };
     }
 
-    function calculateMA(dayCount) {
+    function calculateMA(dayCount, data) {
       var result = [];
-      for (var i = 0, len = data0.values.length; i < len; i++) {
+      for (var i = 0, len = data.values.length; i < len; i++) {
         if (i < dayCount) {
           result.push("-");
           continue;
         }
         var sum = 0;
         for (var j = 0; j < dayCount; j++) {
-          sum += data0.values[i - j][1];
+          sum += data.values[i - j][1];
         }
-        result.push(sum / dayCount);
+        result.push(+(sum / dayCount).toFixed(3));
       }
       return result;
     }
 
-    let option = {
+    var option = {
+      backgroundColor: "#fff",
+      animation: false,
+      legend: {
+        bottom: 10,
+        left: "center",
+        data: ["Dow-Jones index"]
+      },
+      barWidth: 10,
       tooltip: {
         trigger: "axis",
         axisPointer: {
           type: "cross"
+        },
+        backgroundColor: "rgba(245, 245, 245, 0.8)",
+        borderWidth: 1,
+        borderColor: "#ccc",
+        padding: 10,
+        textStyle: {
+          color: "#000"
+        },
+        position: function(pos, params, el, elRect, size) {
+          var obj = { top: 10 };
+          obj[["left", "right"][+(pos[0] < size.viewSize[0] / 2)]] = 30;
+          return obj;
+        },
+        extraCssText: "width: 170px"
+      },
+      axisPointer: {
+        link: { xAxisIndex: "all" },
+        label: {
+          backgroundColor: "#777"
         }
       },
-      legend: {
-        data: ["日K", "MA5", "MA10", "MA20", "MA30"]
-      },
-      grid: {
-        left: "10%",
-        right: "10%",
-        bottom: "15%"
-      },
-      xAxis: {
-        type: "category",
-        data: data0.categoryData,
-        scale: true,
-        boundaryGap: false,
-        axisLine: { onZero: false },
-        splitLine: { show: false },
-        splitNumber: 20,
-        min: "dataMin",
-        max: "dataMax"
-      },
-      yAxis: {
-        position: "right",
-        scale: true,
-        splitArea: {
-          show: true
+      toolbox: {
+        feature: {
+          dataZoom: {
+            yAxisIndex: false
+          },
+          brush: {
+            type: ["lineX", "clear"]
+          }
         }
       },
+      brush: {
+        xAxisIndex: "all",
+        brushLink: "all",
+        outOfBrush: {
+          colorAlpha: 0.1
+        }
+      },
+      visualMap: {
+        show: false,
+        seriesIndex: 5,
+        dimension: 2,
+        pieces: [
+          {
+            value: 1,
+            color: downColor
+          },
+          {
+            value: -1,
+            color: upColor
+          }
+        ]
+      },
+      grid: [
+        {
+          left: "10%",
+          right: "8%",
+          height: "50%"
+        },
+        {
+          left: "10%",
+          right: "8%",
+          top: "63%",
+          height: "16%"
+        }
+      ],
+      xAxis: [
+        {
+          type: "category",
+          data: data.categoryData,
+          scale: true,
+          boundaryGap: false,
+          axisLine: { onZero: false },
+          splitLine: { show: false },
+          splitNumber: 0,
+          min: "dataMin",
+          max: "dataMax",
+          axisPointer: {
+            z: 100
+          }
+        },
+        {
+          type: "category",
+          gridIndex: 1,
+          data: data.categoryData,
+          scale: true,
+          boundaryGap: false,
+          axisLine: { onZero: false },
+          axisTick: { show: false },
+          splitLine: { show: false },
+          axisLabel: { show: false },
+          splitNumber: 20,
+          min: "dataMin",
+          max: "dataMax",
+          axisPointer: {
+            label: {
+              formatter: function(params) {
+                var seriesValue = (params.seriesData[0] || {}).value;
+                return (
+                  params.value +
+                  (seriesValue != null
+                    ? "\n" + echarts.format.addCommas(seriesValue)
+                    : "")
+                );
+              }
+            }
+          }
+        }
+      ],
+      yAxis: [
+        {
+          scale: true,
+          splitArea: {
+            show: true
+          }
+        },
+        {
+          scale: true,
+          gridIndex: 1,
+          splitNumber: 2,
+          axisLabel: { show: false },
+          axisLine: { show: false },
+          axisTick: { show: false },
+          splitLine: { show: false }
+        }
+      ],
       dataZoom: [
         {
           type: "inside",
-          start: 50,
+          xAxisIndex: [0, 1],
+          start: 0,
           end: 100
         },
         {
           show: true,
+          xAxisIndex: [0, 1],
           type: "slider",
-          y: "90%",
-          start: 50,
+          top: "85%",
+          start: 98,
           end: 100
         }
       ],
       series: [
         {
-          name: "日K",
+          name: "Dow-Jones index",
           type: "candlestick",
-          data: data0.values,
+          data: data.values,
           itemStyle: {
             normal: {
               color: upColor,
               color0: downColor,
-              borderColor: upBorderColor,
-              borderColor0: downBorderColor
+              borderColor: null,
+              borderColor0: null
             }
           },
-          markPoint: {
-            label: {
-              normal: {
-                formatter: function(param) {
-                  return param != null ? Math.round(param.value) : "";
-                }
-              }
-            },
-            data: [
-              {
-                name: "XX标点",
-                coord: ["2013/5/31", 2300],
-                value: 2300,
-                itemStyle: {
-                  normal: { color: "rgb(41,60,85)" }
-                }
-              },
-              {
-                name: "highest value",
-                type: "max",
-                valueDim: "highest"
-              },
-              {
-                name: "lowest value",
-                type: "min",
-                valueDim: "lowest"
-              },
-              {
-                name: "average value on close",
-                type: "average",
-                valueDim: "close"
-              }
-            ],
-            tooltip: {
-              formatter: function(param) {
-                return param.name + "<br>" + (param.data.coord || "");
-              }
+          tooltip: {
+            formatter: function(param) {
+              param = param[0];
+              return [
+                "Date: " + param.name + '<hr size=1 style="margin: 3px 0">',
+                "Open: " + param.data[0] + "<br/>",
+                "Close: " + param.data[1] + "<br/>",
+                "Lowest: " + param.data[2] + "<br/>",
+                "Highest: " + param.data[3] + "<br/>"
+              ].join("");
             }
-          },
-          markLine: {
-            symbol: ["none", "none"],
-            data: [
-              [
-                {
-                  name: "from lowest to highest",
-                  type: "min",
-                  valueDim: "lowest",
-                  symbol: "circle",
-                  symbolSize: 10,
-                  label: {
-                    normal: { show: false },
-                    emphasis: { show: false }
-                  }
-                },
-                {
-                  type: "max",
-                  valueDim: "highest",
-                  symbol: "circle",
-                  symbolSize: 10,
-                  label: {
-                    normal: { show: false },
-                    emphasis: { show: false }
-                  }
-                }
-              ],
-              {
-                name: "min line on close",
-                type: "min",
-                valueDim: "close"
-              },
-              {
-                name: "max line on close",
-                type: "max",
-                valueDim: "close"
-              }
-            ]
           }
         },
         {
           name: "MA5",
           type: "line",
-          data: calculateMA(5),
+          data: calculateMA(5, data),
           smooth: true,
           lineStyle: {
             normal: { opacity: 0.5 }
@@ -228,7 +282,7 @@ export default class ParticularOnlineContent extends React.Component {
         {
           name: "MA10",
           type: "line",
-          data: calculateMA(10),
+          data: calculateMA(10, data),
           smooth: true,
           lineStyle: {
             normal: { opacity: 0.5 }
@@ -237,7 +291,7 @@ export default class ParticularOnlineContent extends React.Component {
         {
           name: "MA20",
           type: "line",
-          data: calculateMA(20),
+          data: calculateMA(20, data),
           smooth: true,
           lineStyle: {
             normal: { opacity: 0.5 }
@@ -246,11 +300,18 @@ export default class ParticularOnlineContent extends React.Component {
         {
           name: "MA30",
           type: "line",
-          data: calculateMA(30),
+          data: calculateMA(30, data),
           smooth: true,
           lineStyle: {
             normal: { opacity: 0.5 }
           }
+        },
+        {
+          name: "Volume",
+          type: "bar",
+          xAxisIndex: 1,
+          yAxisIndex: 1,
+          data: data.volumes
         }
       ]
     };
@@ -261,14 +322,16 @@ export default class ParticularOnlineContent extends React.Component {
     if (!nextState.oldData || !nextState.kLoaded) {
       return;
     }
-    let oldKey = Object.keys(nextState.oldData);
     let res = [];
-    nextState.oldData[oldKey[1]].map((item, index) => {
+
+    nextState.oldData.map(item => {
       let r = [];
-      r[0] = this.setTimeString(item[0]);
-      oldKey.map((item1, index1) => {
-        r.push(nextState.oldData[item1][index][1]);
-      });
+      r.push(this.setTimeString(item["time"]));
+      r.push(item["opened_price"]);
+      r.push(item["closed_price"]);
+      r.push(item["min_price"]);
+      r.push(item["max_price"]);
+      r.push(item["volume"]);
       res.push(r);
     });
 
